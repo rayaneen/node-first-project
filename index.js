88d2285a38dc0ffeef80c4e95693f4e553dfb685
Rayane Nordine <rayanenordine@gmail.com>
2022-05-22 18:50:18 +0200

    add port for prod

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const { process_params } = require('express/lib/router');
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const Math = require('mathjs')
 const options = {
